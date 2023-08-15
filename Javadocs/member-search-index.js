4a757af2be057e11e0b98a638f73b9882c65725c
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-08-16 03:48:33 +0530

    add new javadoc

--- Javadocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.shipmentXpress.test","c":"ShipmentXpressTest","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"com.shipmentXpress.test","c":"ShipmentXpressTest","l":"ShipmentXpressTest()","u":"%3Cinit%3E()"}];updateSearchResults();
